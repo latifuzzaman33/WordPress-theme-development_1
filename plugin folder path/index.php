a091d1770132ce81f25c71bb634475f43eb70519
latifuzzaman33 <123368630+latifuzzaman33@users.noreply.github.com>
2023-02-04 00:00:53 +0600

    Add files via upload

---
+++ plugin folder path/index.php
@@ -0,0 +1,19 @@
+<?php 
+?>
+
+    <div class="col-md-3">
+        <div class="single-service">
+            <div class="services-img"> 
+                <img src="<?php echo plugin_dir_url(__FILE__)."img/ser-01.jpg"?>" alt="">
+            </div>
+            <div class="services-content"> 
+                <div class="service-icon"> 
+                    <i class="fa fa-gem"></i>
+                </div>
+                <div class="service-text"> 
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa in saepe quae fuga obcaecati adipisci, esse voluptatum praesentium dignissimos est, soluta repellat dolores reiciendis dolor provident consequatur laboriosam. Voluptatum perspiciatis voluptas saepe doloremque fugiat omnis, molestias quod laudantium cupiditate pariatur.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php 
