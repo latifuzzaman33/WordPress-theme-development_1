af9ca86479b3af5659f8ab672f2ac758d70a2c29
latifuzzaman33 <123368630+latifuzzaman33@users.noreply.github.com>
2023-02-04 00:09:47 +0600

    Add files via upload

---
+++ wordpress custom login system/custom-login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	
+	<?php
+		$login  = (isset($_GET['login']) ) ? $_GET['login'] : 0;
+		
+		
+		if ( $login === "failed" ) {
+			
+		  echo '<p class="login-msg"><strong>ERROR:</strong> Invalid username and/or password.</p>';
+		  
+		} elseif ( $login === "empty" ) {
+			
+		  echo '<p class="login-msg"><strong>ERROR:</strong> Username and/or Password is empty.</p>';
+		  
+		} elseif ( $login === "false" ) {
+			
+		  echo '<p class="login-msg"><strong>ERROR:</strong> You are logged out.</p>';
+		}
+
+	?>
+	
+	
+	
+	
+	<?php
+	$args = array(
+		'redirect' => home_url(), 
+		'id_username' => 'user',
+		'id_password' => 'pass',
+	   ) 
+	;?>
+	<?php wp_login_form( $args ); ?>
+
+
+
+	
+</body>
+</html>
